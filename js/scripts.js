30dcbfa18f99ece05f61de3630fd738da1f4049a
Salah Kaifas <salah.kaifas@gmail.com>
2021-08-04 17:43:36 +0100

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+<script>
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval:2000});
+    $('#loginbtn').click(function(){
+        $('#loginModal').modal('toggle')
+    });
+    $('#reserve-table-btn').click(function(){
+        $('#reserve-table').modal('toggle')
+    });
+    
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+})
+</script>
